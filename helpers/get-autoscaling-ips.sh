86d2d5affb8bbd363f509fe0045e284b33251f11
Shirish Nigam <nigamshirish@gmail.com>
2020-05-24 18:55:19 +1000

    Cleanup comment in script

--- helpers/get-autoscaling-ips.sh
+++ helpers/get-autoscaling-ips.sh
@@ -10,6 +10,6 @@ else
     profile=''
 fi
 
-instanceIds=`aws autoscaling describe-auto-scaling-instances --query 'AutoScalingInstances[*].InstanceId' --output text $profile` #--profile=coronatracker`
-publicIps=`aws ec2 describe-instances --instance-ids ${instanceIds} --query 'Reservations[*].Instances[*].PublicIpAddress' --output text $profile` #--profile=coronatracker`
+instanceIds=`aws autoscaling describe-auto-scaling-instances --query 'AutoScalingInstances[*].InstanceId' --output text $profile`
+publicIps=`aws ec2 describe-instances --instance-ids ${instanceIds} --query 'Reservations[*].Instances[*].PublicIpAddress' --output text $profile`
 echo $publicIps

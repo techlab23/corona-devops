37dad454f723c343df29f90da5c19915f79cb3a3
Shirish Nigam <nigamshirish@gmail.com>
2020-05-24 18:52:24 +1000

    Add helper scripts

---
+++ helpers/get-autoscaling-ips.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Use custom profile name if applicable otherwise leave it blank. 
+#Usage: ./get-autoscaling-ips AWSProfileName
+
+if [ $1 ]
+then
+    profile=`echo --profile=$1`
+else
+    profile=''
+fi
+
+instanceIds=`aws autoscaling describe-auto-scaling-instances --query 'AutoScalingInstances[*].InstanceId' --output text $profile` #--profile=coronatracker`
+publicIps=`aws ec2 describe-instances --instance-ids ${instanceIds} --query 'Reservations[*].Instances[*].PublicIpAddress' --output text $profile` #--profile=coronatracker`
+echo $publicIps
